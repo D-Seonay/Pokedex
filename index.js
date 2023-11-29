195726c54c9252f0e74441fe2bc63c273c2eb1bd
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-29 11:50:14 +0100

    Update server port and add fetch request for Pokemon data

--- index.js
+++ index.js
@@ -7,9 +7,20 @@ const routesPokemon = require('./routes/routesPokemon');
 // Utiliser les routes
 app.use('/', routesPokemon);
 
+fetch('/fetchPokemon', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+.then(response => response.json())
+.then(data => console.log(data))
+.catch((error) => {
+    console.error('Erreur:', error);
+});
 
 // Démarrer le serveur
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
     console.log(`Serveur lancé sur le port ${port} http://localhost:${port}`);
 });
